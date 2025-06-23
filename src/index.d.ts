1acb8677facdf933a9f3de4b6320c7a9d6f62a79
Gabriel Montes <gabriel@hemi.xyz>
2025-06-23 13:58:12 -0400

    Add types

---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Fetches JSON data from a URL.
+ * @param resource The resource to fetch, i.e. the URL.
+ * @param options Additional settings to apply to the request.
+ * @returns The response parsed as JSON.
+ */
+declare function fetchJson(
+  resource: string,
+  options?: RequestInit,
+): Promise<any>;
+
+export = fetchJson;
